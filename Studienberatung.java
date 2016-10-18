7d9557dd3bc4364a0daf84dc18436767249c6e43
Murat Yilmaz <murat.yil90@gmail.com>
2016-10-18 17:26:32 +0200

    Frage Hast du Abitur

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Studienberatung {
 	
 	public static void main(String [] args){
 		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Hast du Abitur?");
+		String a1 = scanner.next();
 		
 	}
 }
