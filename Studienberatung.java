bf513da07ef7244ebc2ea25b65c591af943e1293
Murat Yilmaz <murat.yil90@gmail.com>
2016-10-18 17:39:08 +0200

    Frage Bist du wissbegierig

--- Studienberatung.java
+++ Studienberatung.java
@@ -8,5 +8,17 @@ public class Studienberatung {
 		System.out.println("Hast du Abitur?");
 		String a1 = scanner.next();
 		
+		if(a1.equals("j")){
+			System.out.println("Bist du wissbegierig?");
+			String a2 = scanner.next();
+			
+			if (a2.equals("j")){
+				System.out.println("Auf jedenfall studieren!");	
+			}
+			else if (a2.equals("n")){
+				System.out.println("Nicht studieren");
+			}
+			
+		}
 	}
 }
